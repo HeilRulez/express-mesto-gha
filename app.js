ce07aaae18e5fe9cc3ba8ded70bce1a8bc5a6c9c
HeilRulez <Rulez.Heil@gmail.com>
2022-09-04 20:49:19 +0600

    правки под тесты

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: false
 });
 
+app.use('/', (req, res, next) => {
+  res.status(404).send({ message: 'Страница не найдена.'});
+  next();
+});
 app.use((req, res, next) => {
   req.user = {
     _id: '63118c6481ba22e901e5e83f'
@@ -18,11 +22,6 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-
-app.use('/', (req, res, next) => {
-  res.status(404).send({ message: 'Страница не найдена.'});
-  next();
-});
 app.use(routesUser);
 app.use(routesCards);
 
