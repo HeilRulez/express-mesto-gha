9c9c23fda3b9ce4833726a58a1f9af5cf718cd42
HeilRulez <Rulez.Heil@gmail.com>
2022-09-04 20:43:06 +0600

    правки под тесты

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.use('/', (req, res, next) => {
-  res.status(404).send({ message: 'Страница не найдена.'})
+  res.status(404).send({ message: 'Страница не найдена.'})ж
+  next();
 });
 app.use(routesUser);
 app.use(routesCards);
