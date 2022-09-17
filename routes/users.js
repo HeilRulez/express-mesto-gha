b128914e659d2b0073ff82b2a9255d7a3b3194f4
HeilRulez <rulez.heil@gmail.com>
2022-09-17 22:27:21 +0600

    перед тестами

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,7 @@ const {
 } = require('../controllers/users');
 
 routes.get('/', getUsers);
-routes.get('/me', celebrate({
-  user: Joi.object().keys({
-    _id: Joi.string().alphanum().length(24),
-  }),
-}), getUser);
+routes.get('/me', getUser);
 routes.get('/:userId', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().alphanum().length(24),
@@ -20,16 +16,10 @@ routes.patch('/me', celebrate({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
   }),
-  user: Joi.object().keys({
-    _id: Joi.string().alphanum().length(24),
-  }),
 }), updateProfile);
 routes.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().dataUri(),
-  }),
-  user: Joi.object().keys({
-    _id: Joi.string().alphanum().length(24),
   }),
 }), updateAvatar);
 
